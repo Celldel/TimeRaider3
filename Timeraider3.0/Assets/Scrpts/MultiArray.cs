47dc98615f1346003a676b457dd9198070712aa3
Celldel <Mario_strand@hotmail.com>
2015-12-28 09:53:27 +0100

    Revert "First of Two Projects"
    This reverts commit fb72e0027a7e538d87259879a6629eefec1dcb78.

--- Timeraider3.0/Assets/Scrpts/MultiArray.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-
-[System.Serializable]
-public class MultiArray : MonoBehaviour {
-
-	public GameObject[] scoreCounterParents = new GameObject[10];
-}
