1f2d1619406cf03cc3afa35e1d5611edd16116bc
Celldel <Mario_strand@hotmail.com>
2015-12-28 10:02:44 +0100

    Hugos del som antagligen inte är prefa...
    dsadasd

---
+++ Timeraider3.0/Assets/HugoSCript/Scrpts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public int bulletSpeed;
+	public float travelTime;
+	Vector3 spawnPoint;
+	bool shooting;
+	float time;
+	public float startDelay;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		spawnPoint = gameObject.transform.position;
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		time += Time.deltaTime;
+		transform.Translate (Vector3.right * Time.deltaTime * bulletSpeed);
+
+
+
+		if (time > travelTime && shooting) {
+			transform.position = spawnPoint;
+			time = 0;
+		}
+
+	}
+
+
+}
